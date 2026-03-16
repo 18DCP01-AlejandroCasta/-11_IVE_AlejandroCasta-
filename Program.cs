452d61bfa3d91275fd4faaf5538e3a390834cb32
Ale <d.castaneda.5cdb@gmail.com>
2026-03-16 12:18:59 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Numeros Positivos y Negativos");
+
+        double num;
+        Console.Write("Ingrese un numero entero");
+        num = Convert.ToDouble(Console.ReadLine());
+
+        if (num>0)
+        {
+            Console.WriteLine("Soy numero positivo w");
+        }
+        else
+        {
+            Console.WriteLine("Soy numero negativo w");
+        }
+    }
+}
