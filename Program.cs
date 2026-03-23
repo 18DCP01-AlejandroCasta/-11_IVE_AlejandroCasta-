64a6ae876c213baf18fc4391c6a7409a5af86940
Ale <d.castaneda.5cdb@gmail.com>
2026-03-23 11:34:22 -0600

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -2,19 +2,42 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Numeros Positivos y Negativos");
-
-        double num;
-        Console.Write("Ingrese un numero entero");
-        num = Convert.ToDouble(Console.ReadLine());
-
-        if (num>0)
+        Console.WriteLine("===MENU PRINCIPAL===");
+        //Variables
+        int opc;
+        //Crear Menu
+        Console.Write("MENU PRINCIPAL\n");
+        Console.Write("1- Recepcion\n");
+        Console.Write("2- Vigilancia\n");
+        Console.Write("3- Mantenimiento\n");
+        Console.Write("4- Salida\n");
+        Console.Write("Digite una opcion [ ]: ");
+        opc=Convert.ToInt32(Console.ReadLine());
+        //Condiciones
+        if (opc == 1)
         {
-            Console.WriteLine("Soy numero positivo w");
+            //limpiar pantalla
+            Console.Clear();
+            Console.WriteLine("Bienvenido al depto. de Recepcion");
         }
-        else
+        if (opc == 2)
         {
-            Console.WriteLine("Soy numero negativo w");
+            //limpiar pantalla
+            Console.Clear();
+            Console.WriteLine("Bienvenido al depto. de Vigilancia");
         }
+        if (opc == 3)
+        {
+            //limpiar pantalla
+            Console.Clear();
+            Console.WriteLine("Bienvenido al depto. de Mantenimiento");
+        }
+        if (opc!= 1 && opc!=2 && opc!=3)
+        {
+            //limpiar pantalla
+            Console.Clear();
+            Console.WriteLine("Saliendo del sistema...");
+        }
+
     }
 }
