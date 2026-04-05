139e245fb93c0f5c5520dde135ce5f449d58c8f4
Ale <d.castaneda.5cdb@gmail.com>
2026-04-04 20:10:35 -0600

    Add files via upload

---
+++ act4_lecc20.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("ACTIVIDAD 4 LECCION 20");
+
+        Console.Write("Ingrese la contraseña: ");
+        string password = Console.ReadLine();
+
+        if (password == "Password123")
+            Console.WriteLine("Bienvenido");
+        else
+            Console.WriteLine("Acceso prohibido");
+
+    }
+}
