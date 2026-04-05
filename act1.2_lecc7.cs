d5be58a30c3324bb69f0bf63906b7d673b2c4325
Ale <d.castaneda.5cdb@gmail.com>
2026-04-04 18:17:01 -0600

    Add files via upload

---
+++ act1.2_lecc7.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("==ACTIVIDAD 1.2 LECCION 7===\n");
+
+        Console.WriteLine("Inserte su edad");
+        int e = Convert.ToInt32(Console.ReadLine());
+
+        if (e >= 18)
+        {
+            Console.WriteLine("Usted es una persona legal\n");
+        }
+        else
+        {
+            Console.WriteLine("Usted es menor");
+        }
+    }
+}
