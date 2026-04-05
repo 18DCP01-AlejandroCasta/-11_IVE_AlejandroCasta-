2388fee83ff1382c28914f041e7c2be3cef1e16c
Ale <d.castaneda.5cdb@gmail.com>
2026-04-04 19:47:39 -0600

    Add files via upload

---
+++ act3.1_lecc7.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("===ACTIVIDAD 3.1 LECCION 7===\n");
+
+        Console.WriteLine("Ingrese el lado 1 del triangulo\n");
+        int l1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Ingrese el lado 2 del triangulo\n");
+        int l2 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Ingrese el lado 3 del triangulo\n");
+        int l3 = Convert.ToInt32(Console.ReadLine());
+
+        if (l1 == l2 && l2 == l3)
+            Console.WriteLine("Triángulo equilátero");
+        if (l1 == l2 || l1 == l3 || l2 == l3)
+            Console.WriteLine("Triángulo isósceles");
+        else
+            Console.WriteLine("Triángulo escaleno");
+
+    }
+}
