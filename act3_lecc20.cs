7c96b5899d94ab75d6ebe40e8a64ab0ca22a380c
Ale <d.castaneda.5cdb@gmail.com>
2026-04-04 20:19:57 -0600

    Add files via upload

---
+++ act3_lecc20.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Ingrese un número: ");
+        int dia = int.Parse(Console.ReadLine());
+
+        if (dia == 6 || dia == 7)
+            Console.WriteLine("Fin de semana");
+        else
+            Console.WriteLine("Entre semana");
+    }
+}
